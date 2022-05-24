75dbc672b45bc88fe3ab7ab6c8e1a05ccfde2ed7
Martín <magadiflo@gmail.com>
2022-05-24 18:59:13 -0500

    Cambio de password enviado desde Angular

--- src/app/changePassword/change-password/change-password.component.ts
+++ src/app/changePassword/change-password/change-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { EmailPasswordService } from '../../service/email-password.service';
+import { ChangePasswordDTO } from '../../models/change-password-dto';
 
 @Component({
   selector: 'app-change-password',
@@ -7,13 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  password: string;
+  passwordConfirm: string;
+  tokenPassword: string;
+
+  dto: ChangePasswordDTO;
+
+  constructor(
+    private emailPasswordService: EmailPasswordService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
-  onSendEmail(): void {
-    
+  onChangePassword(): void {
+    if (this.password !== this.passwordConfirm) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+    this.tokenPassword = this.activatedRoute.snapshot.params.tokenPassword;
+    this.dto = new ChangePasswordDTO(this.password, this.passwordConfirm, this.tokenPassword);
+    this.emailPasswordService.changePassword(this.dto)
+      .subscribe(
+        data => {
+          alert('Éxito: ' + data.mensaje);
+          this.router.navigate(['/login']);
+        },
+        err => {
+          console.log(err);
+          alert('Error: ' + err.error.mensaje);
+          this.router.navigate(['/']);
+        });
   }
 
 }
